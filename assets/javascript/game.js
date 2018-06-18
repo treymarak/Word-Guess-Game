20d2e8512673bbb199f4bc0ab523b431ce33c0ba
Trey Marak <treymarak@alfreds-mbp.attlocal.net>
2018-06-18 15:09:33 -0500

    adding java

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var questions = [
+    { q: "Where do the Simpson's live?", a: "Springfield" },
+    { q: "What does Homer drink?", a: "Duff Beer" },
+    { q: "Who is Bart's bestfriend?", a: "Milhouse" },
+    { q: "What is the name of Bart's dog?", a: "Santa's Little Helper" },
+    { q: "Who shot Mr.s Burns?", a: "Maggie Simpson" },
+    { q: "What instrument does Lisa play?", a: "Saxophone" },
+    { q: "Who is always trying to kill Bart?", a: "Sideshow Bob" },
+    { q: "What is the name of the Simpson's next door neighbor?", a: "Ned Flanders" },
+    { q: "What is the name of the bar where Homer drinks?", a: "Moe's Tavern" },
+    { q: "Who is Mr Burns' assistant?", a: "Waylon Smithers" }
+  ];
